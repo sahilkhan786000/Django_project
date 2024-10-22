8a9ccdf92e9f850933c61e7ad2374d4893e29073
Sahil Khan <95959159+sahilkhan786000@users.noreply.github.com>
2024-10-22 11:37:58 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -16,6 +16,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SignupSerializer, LoginSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class SignupView(APIView):
     def post(self, request):
@@ -32,3 +33,11 @@ class LoginView(APIView):
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)  # Return the tokens here
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user  # Get the authenticated user
+        serializer = UserDetailsSerializer(user)
+        return Response(serializer.data)
