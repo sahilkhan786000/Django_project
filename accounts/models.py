61154a690aaf573b66be632077abba5bea3aa1bb
Sahil Khan <95959159+sahilkhan786000@users.noreply.github.com>
2024-10-22 11:48:35 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,13 @@
 import mongoengine as me
-from mongoengine import Document, StringField, EmailField
+from mongoengine import Document, StringField, EmailField, DateTimeField
 from django.contrib.auth.hashers import check_password
-
+from datetime import datetime
 
 class UserDetails(Document):
-   username = StringField(required=True, unique=True)
-   email = StringField(required=True, unique=True)
-   password = StringField(required=True)
-   created_at = DateTimeField(default=datetime.utcnow)
+    username = StringField(required=True, unique=True)
+    email = EmailField(required=True, unique=True)  # Use EmailField for email
+    password = StringField(required=True)
+    created_at = DateTimeField(default=datetime.utcnow)
 
     def check_password(self, raw_password):
         return check_password(raw_password, self.password)
