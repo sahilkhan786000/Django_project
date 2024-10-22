99e62b28f772beb9a725c3920ee7404b2137c982
Sahil Khan <95959159+sahilkhan786000@users.noreply.github.com>
2024-10-22 14:26:08 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import SignupSerializer, LoginSerializer
+from .serializers import SignupSerializer, LoginSerializer, UserDetailsSerializer
 from rest_framework.permissions import IsAuthenticated
 import logging
 from .models import UserDetails
