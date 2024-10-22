0ab0af4c68c82725ddc6b2fc5bbe1b9a5f915f81
Sahil Khan <95959159+sahilkhan786000@users.noreply.github.com>
2024-10-22 14:03:44 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -17,6 +17,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SignupSerializer, LoginSerializer
 from rest_framework.permissions import IsAuthenticated
+import logging
 
 class SignupView(APIView):
     def post(self, request):
@@ -34,10 +35,16 @@ class LoginView(APIView):
             return Response(serializer.validated_data, status=status.HTTP_200_OK)  # Return the tokens here
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+logger = logging.getLogger(__name__)
+
 class UserDetailView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        user = request.user  # Get the authenticated user
-        serializer = UserDetailsSerializer(user)
-        return Response(serializer.data)
+        logger.info(f"User: {request.user}")  # Log the user associated with the request
+        if request.user.is_authenticated:
+            serializer = UserDetailsSerializer(request.user)
+            return Response(serializer.data)
+        else:
+            logger.error("User is not authenticated.")
+            return Response({"detail": "Authentication credentials were not provided."}, status=401)
